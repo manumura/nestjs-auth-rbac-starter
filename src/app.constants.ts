b7a69d9b02590fd37b3624d7cc83f4ae7359e4b9
Emmanuel Mura <emmanuel.mura@gmail.com>
2023-03-06 10:53:02 +0700

    update templates

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,13 +1,13 @@
 export const appConstants = {
   RESET_PASSWORD_EMAIL_SUBJECT: {
-    en: 'Reset your password!',
-    fr: 'Réinitialisez votre mot de passe !',
+    en: 'Reset your MyApp password!',
+    fr: 'Réinitialisez votre mot de passe MyApp!',
   },
   RESET_PASSWORD_EMAIL_TEMPLATE: 'reset-password',
 
   TEMPORARY_PASSWORD_EMAIL_SUBJECT: {
-    en: 'Your password!',
-    fr: 'Votre mot de passe !',
+    en: 'Your MyApp password!',
+    fr: 'Votre mot de passe MyApp!',
   },
   TEMPORARY_PASSWORD_EMAIL_TEMPLATE: 'temporary-password',
 };
