863dab97eb42018adb8995db27173f0b89a5d4c4
Emmanuel Mura <emmanuel.mura@gmail.com>
2023-07-23 11:25:30 +0700

    idToken cookie

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -13,4 +13,5 @@ export const appConstants = {
   ACCESS_TOKEN: 'accessToken',
   REFRESH_TOKEN: 'refreshToken',
   ACCESS_TOKEN_EXPIRES_AT: 'accessTokenExpiresAt',
+  ID_TOKEN: 'idToken',
 };
--- src/shared/util/cookies.ts
+++ src/shared/util/cookies.ts
@@ -8,10 +8,12 @@ export const setAuthCookies = (response: FastifyReply, login: LoginModel) => {
   response.setCookie(appConstants.ACCESS_TOKEN, login.accessToken, COOKIE_OPTIONS);
   response.setCookie(appConstants.ACCESS_TOKEN_EXPIRES_AT, moment(login.accessTokenExpiresAt).format(), COOKIE_OPTIONS);
   response.setCookie(appConstants.REFRESH_TOKEN, login.refreshToken, COOKIE_OPTIONS);
+  response.setCookie(appConstants.ID_TOKEN, login.idToken, COOKIE_OPTIONS);
 };
 
 export const clearAuthCookies = (response: FastifyReply) => {
   response.clearCookie(appConstants.ACCESS_TOKEN, COOKIE_OPTIONS);
   response.clearCookie(appConstants.ACCESS_TOKEN_EXPIRES_AT, COOKIE_OPTIONS);
   response.clearCookie(appConstants.REFRESH_TOKEN, COOKIE_OPTIONS);
+  response.clearCookie(appConstants.ID_TOKEN, COOKIE_OPTIONS);
 };
