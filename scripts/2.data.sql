4a489640c448eb281cfcc8ea2ebacd7ad0567293
Emmanuel Mura <emmanuel.mura@gmail.com>
2024-10-11 17:08:46 +0700

    init data

--- scripts/2.data.sql
+++ scripts/2.data.sql
@@ -9,10 +9,10 @@ VALUES
 ('USER', 'Role User');
 
 INSERT INTO "user"
-(uuid, "name", is_active, created_at, updated_at, role_id)
+(uuid, "name", is_active, is_email_verified, created_at, updated_at, role_id)
 VALUES
-('9153ec44-6052-4bcd-80d8-7462cffe99fb', 'admin', true, NOW(), NOW(), 1),
-('816e0d9e-9cfe-4191-9d3b-58f27c28d3d7', 'manolo', true, NOW(), NOW(), 1);
+('9153ec44-6052-4bcd-80d8-7462cffe99fb', 'admin', true, true, NOW(), NOW(), 1),
+('816e0d9e-9cfe-4191-9d3b-58f27c28d3d7', 'manolo', true, true, NOW(), NOW(), 1);
 
 -- pwd = 12345678
 INSERT INTO "user_credentials"
